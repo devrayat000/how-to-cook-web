e55a8041d6c8bc2552460769d7f1454655d31171
Zul Ikram Musaddik Rayat <rayathos04@gmail.com>
2025-02-24 08:23:38 +0600

    added docker publishing

--- HowToCook.Server/Program.cs
+++ HowToCook.Server/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<RecipeDbContext>(options =>
     options
-        .UseSqlite(builder.Configuration.GetConnectionString("DatabaseConnection"))
+        .UseSqlite(Environment.GetEnvironmentVariable("DATABASE_CONNECTION"))
         //.UseSeeding((context, _) => RecipeDbContext.SeedData(context))
 );
 
